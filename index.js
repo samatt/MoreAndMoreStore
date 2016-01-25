9f7c24df42f9cc3a77b5045a51df3097fa587108
Surya Mattu <surya@suryamattu.com>
2016-01-24 22:00:36 -0500

    sdasd

--- index.js
+++ index.js
@@ -62,7 +62,8 @@ function getProducts(src,dst,cb){
 
 		  		};
 		  		console.log( 'missing',missing);
-		  		cb.json({'icons':eliminateDuplicates(icons),'missing':missing});
+		  		cb.json(eliminateDuplicates(icons));
+		  		// cb.json({'icons':eliminateDuplicates(icons),'missing':missing});
 		  }
 		})
 	console.log("here");
